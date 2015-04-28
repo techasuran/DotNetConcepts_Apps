3185a9486b28b386cb3335fe2036514ff31301dd
Nevin Mathew <nevviin@gmail.com>
2015-04-28 20:46:57 +0530

    adding simple inheritance

---
+++ SimpleInheritance/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInheritance
+{
+
+    class BaseClass
+    {
+        public  BaseClass()
+        {
+            Console.WriteLine("constructor BaseClass");
+        }
+    }
+    class ChildClass : BaseClass
+    {
+        public  ChildClass()
+        {
+            Console.WriteLine("Constructor ChildClass");
+        }
+    }
+}
---
+++ SimpleInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Console.WriteLine("Creating object of child class and assign to childclass object");
+            ChildClass childClassObj = new ChildClass();
+
+            Console.WriteLine("\n\n");
+
+            //Console.WriteLine("Creating object of child class and assigning to baseclass object");
+            BaseClass baseClassObj = new ChildClass();
+
+            Console.ReadLine();
+
+        }
+    }
+}
